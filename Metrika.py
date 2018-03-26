88bc5dcee55c3a068cb9b8f624af3bd3a8e7173d
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-03-27 01:34:01 +0300

    delete password

--- Metrika.py
+++ Metrika.py
@@ -11,9 +11,7 @@ from urllib.parse import urlencode
 #
 # Получение статистики, чтение параметров своих и доверенных счётчиков
 #
-# ID: e95478f3c0b84ac38835491d5af6dd1d
-# Пароль: f2f90796f1d24ec182b0239381e832c7
-# Callback URL: https://oauth.yandex.ru/verification_code
+
 
 # APP_ID = 'e95478f3c0b84ac38835491d5af6dd1d'
 # AUTH_URL = 'https://oauth.yandex.ru/authorize'
